b8ac6bed81b3e473ee12c1a31037f94d4ae012a6
Andrest31 <128243532+Andrest31@users.noreply.github.com>
2025-04-21 18:12:34 +0300

    создать на всех подставляет список пользователей

--- client/AviTaska/src/App.tsx
+++ client/AviTaska/src/App.tsx
@@ -4,6 +4,7 @@ import BoardPage from "./pages/BoardPage/BoardPage";
 import IssuesPage from "./pages/IssuesPage/IssuesPage";
 import Header from "./components/Header/Header";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
@@ -13,13 +14,37 @@ function App() {
   );
 }
 
+interface Assignee {
+  id: number;
+  fullName: string;
+  email: string;
+  avatarUrl: string;
+}
+
 function AppWithHeader() {
   const location = useLocation();
   const isBoardPage = location.pathname.startsWith("/board/");
 
+  const [allAssignees, setAllAssignees] = useState<Assignee[]>([]);
+
+  useEffect(() => {
+    const fetchAssignees = async () => {
+      try {
+        const assigneesResponse = await fetch('http://localhost:8080/api/v1/users');
+        if (!assigneesResponse.ok) throw new Error('Ошибка загрузки пользователей');
+        const assigneesData = await assigneesResponse.json();
+        setAllAssignees(assigneesData.data || []);
+      } catch (err) {
+        console.error('Ошибка загрузки пользователей:', err);
+      }
+    };
+
+    fetchAssignees();
+  }, []);
+
   return (
     <div className="app">
-      {!isBoardPage && <Header />} {/* не показываем Header на /boards */}
+      {!isBoardPage && <Header allAssignees={allAssignees}/>} {/* не показываем Header на /boards */}
       <Routes>
         <Route path="/boards" element={<BoardsPage />} />
         <Route path="/board/:id" element={<BoardPage />} />
